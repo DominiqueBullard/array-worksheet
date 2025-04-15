6ee243ff41b773630a528df7dba167a1df782933
DominiqueBullard <dominique.miller21@gmail.com>
2025-04-15 03:31:35 +0000

    aahhh

---
+++ script.js
@@ -0,0 +1,22 @@
+const fruit=[];
+const  newFruit =["banana", "cherry", "apple"];
+
+console.log(fruit.legth)
+[3]
+
+// const  newFruit2 ["orange"];
+fruit.push["orange", "peach"];
+
+console.log(newFruit);
+
+const fullFruit = ["banana", "cherry", "apple", "orange", "peach"];
+const popFruit = fullFruit.pop();
+
+console.log(popFruit)
+console.log(fullFruit)
+
+const fullFruit = ["banana", "cherry", "apple", "orange", "peach"];
+ for (let 1 = 0; i < array.length; i++) {
+    const element = array[i];
+    
+ }
